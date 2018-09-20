681b56d7e1d05b99eea8714793f30fc1d304bf93
plee19 <plee19@my.wctc.edu>
2018-09-19 20:18:34 -0500

    Final completion with Javadoc.

--- src/us/plee19/numwrappers/Main.java
+++ src/us/plee19/numwrappers/Main.java
@@ -1,9 +1,10 @@
 package us.plee19.numwrappers;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /**
- * Main method to illustrate use of number wrappers.
+ * Main method to illustrate use of numeric wrappers.
  * @author plee19
  * @version 1
  */
@@ -14,9 +15,30 @@ public class Main {
         System.out.print("Please enter a number: ");
         Scanner keyboard = new Scanner(System.in);
         String input = keyboard.nextLine();
+        getDataType(input);
     }
 
-    //public static void getDataType(String input) {
-    //   if ()
-    //}
+    public static void getDataType(String input) {
+
+        try {
+            long wholeNumber = Long.parseLong(input);
+            if (wholeNumber < Byte.MAX_VALUE && wholeNumber > Byte.MIN_VALUE) {
+                System.out.println("Byte");
+            } else if (wholeNumber < Short.MAX_VALUE && wholeNumber > Short.MIN_VALUE) {
+                System.out.println("Short");
+            } else if (wholeNumber < Integer.MAX_VALUE && wholeNumber > Integer.MIN_VALUE) {
+                System.out.println("Integer");
+            } else {
+                System.out.println("Long");
+            }
+        } catch (NumberFormatException e) {
+            double decimalNumber = Double.parseDouble(input);
+
+            if (decimalNumber < Float.MAX_VALUE && decimalNumber > Float.MIN_VALUE) {
+                System.out.println("Float");
+            } else {
+                System.out.println("Double");
+            }
+        }
+    }
 }
