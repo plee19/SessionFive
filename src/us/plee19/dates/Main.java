a113607219438279088926dcb86d7e444a2e0732
plee19 <plee19@my.wctc.edu>
2018-09-19 19:49:26 -0500

    Completed dates package

--- src/us/plee19/dates/Main.java
+++ src/us/plee19/dates/Main.java
@@ -1,5 +1,7 @@
 package us.plee19.dates;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -13,8 +15,17 @@ public class Main {
     public static void main(String[] args) {
         Date firstDate = new GregorianCalendar(1987, 3, 9).getTime();
         Date secondDate = new GregorianCalendar(1988, 10, 10).getTime();
+        Date today = new Date();
         long dateDiff = secondDate.getTime() - firstDate.getTime();
-        long daysDiff = dateDiff / (1000 * 60 * 60 * 24);
+        int daysDiff = (int)(dateDiff / (1000 * 60 * 60 * 24));
+        System.out.println("Days between album releases: " + daysDiff);
 
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, daysDiff);
+        String nextAlbum = sdf.format(c.getTime());
+        System.out.println("Today's date is: " + sdf.format(today));
+        System.out.println("Next album release is: " + nextAlbum);
     }
 }
