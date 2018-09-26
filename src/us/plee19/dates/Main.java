f52586c3d0aebb431686d0afb774a505bdf92af1
plee19 <plee19@my.wctc.edu>
2018-09-26 09:34:41 -0500

    Removed additional method.

--- src/us/plee19/dates/Main.java
+++ src/us/plee19/dates/Main.java
@@ -13,13 +13,20 @@ import java.util.GregorianCalendar;
 public class Main {
 
     public static void main(String[] args) {
-        Date firstDate = new GregorianCalendar(1987, 3, 9).getTime();
-        Date secondDate = new GregorianCalendar(1988, 10, 10).getTime();
+        Date firstDate = new GregorianCalendar(1987, 2, 9).getTime();
+        Date secondDate = new GregorianCalendar(1988, 9, 10).getTime();
         Date today = new Date();
+        long todayDiff = today.getTime() - firstDate.getTime();
+        int daysFromTodayDiff = (int)(todayDiff / (1000 * 60 * 60 * 24));
+        System.out.println("Days since Joshua Tree: " + daysFromTodayDiff);
         long dateDiff = secondDate.getTime() - firstDate.getTime();
         int daysDiff = (int)(dateDiff / (1000 * 60 * 60 * 24));
         System.out.println("Days between album releases: " + daysDiff);
-
+        int years = daysFromTodayDiff / 365;
+        int dayRemainder = daysFromTodayDiff - (years * 365);
+        int months = dayRemainder / 30;
+        dayRemainder = dayRemainder - (months * 30);
+        System.out.println("Joshua Tree was released " + years + " years, " + months + " months, and " + dayRemainder + " days ago.");
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         Calendar c = Calendar.getInstance();
         c.setTime(new Date());
