f0973d29f44c27e33784de956e864d5eca19ee95
plee19 <plee19@my.wctc.edu>
2018-09-26 09:35:49 -0500

    Removed additional method.

--- src/us/plee19/numwrappers/Main.java
+++ src/us/plee19/numwrappers/Main.java
@@ -1,6 +1,5 @@
 package us.plee19.numwrappers;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 /**
@@ -9,16 +8,11 @@ import java.util.Scanner;
  * @version 1
  */
 public class Main {
-    String input;
 
     public static void main(String[] args) {
         System.out.print("Please enter a number: ");
         Scanner keyboard = new Scanner(System.in);
         String input = keyboard.nextLine();
-        getDataType(input);
-    }
-
-    public static void getDataType(String input) {
 
         try {
             long wholeNumber = Long.parseLong(input);
